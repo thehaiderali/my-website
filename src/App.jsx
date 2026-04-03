7b40907b4452e4cfa709458e0222524cb74336bf
Haider Ali <hyder6291@gmail.com>
2026-04-03 16:54:13 +0500

    Portfolio Rebuild

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full min-h-screen flex justify-center items-center bg-foreground '>
+      <p className='text-4xl font-mono text-background'>React + Tailwind + Shadcn Template {"(Javascript Version)"} </p>
+      </div>
+  )
+}
+
+export default App
